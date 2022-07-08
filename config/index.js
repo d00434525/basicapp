d0500b4bd2d1738808b607040ae2bef1640852d3
d00434525 <d00434525@dmail.dixie.edu>
2022-07-08 10:11:35 -0600

    added config folder with index

---
+++ config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongo_user: "new_user",
+    mongo_pass: "password",
+    mongo_port: 27018,
+    mongo_host: "127.0.0.1",
+    mongo_db: "basicform",
+    http_port: 5050,
+};
