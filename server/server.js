3360c48440bf51a6df494a2be0d7bc7de46334b4
d00434525 <d00434525@dmail.dixie.edu>
2022-07-08 10:37:58 -0600

    added post user

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const { User } = require("../persist/model");
 const setUpAuth = require("./auth");
-const setUpSessionStore = require("./session");
-const setUpAuth = require("./session");
+const setUpSession = require("./session");
 const app = express();
 
 // tell the server to understand how to read json
 app.use(express.json());
 
-setUpSessionStore(app);
+setUpSession(app);
 setUpAuth(app);
+
+app.post("/users", async (req,res) => {
+    try {
+        let user = await User.create({
+            username: req.body.username,
+            fullname: req.body.fullname,
+            password: req.body.password,
+        });
+        res.status(201).json(user);
+    } catch {
+        res.status(500).json({
+            message: `post request failed to create user`,
+            error: error,
+        });
+    }
+});
+
 
 module.exports = app;
