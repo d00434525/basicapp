ef85984278c502d03840fdc71e78a53f439665e2
d00434525 <d00434525@dmail.dixie.edu>
2022-07-08 10:09:07 -0600

    added index.js

---
+++ persist/model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+    {
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        body: {
+            type: String,
+            required: true,
+            default: "",
+        },
+        thread_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Thread",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullname: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+};
